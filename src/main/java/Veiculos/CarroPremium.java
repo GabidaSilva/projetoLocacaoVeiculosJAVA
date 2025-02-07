122a130dd5c6ab1fe98aed8d0417bb6fad1a116d
owie <marketing@letscode.com.br>
2025-02-07 17:51:34 -0300

    fix: Adicionado dados CarroPremium e SUV

--- src/main/java/Veiculos/CarroPremium.java
+++ src/main/java/Veiculos/CarroPremium.java
@@ -1,8 +1,19 @@
 package Veiculos;
 
 public class CarroPremium extends Veiculo implements ServicoLuxo {
+
+    private boolean cambioAutomatico;
+    private boolean computadorBordo;
+
     public CarroPremium(String modelo, String placa, double valorDiaria){
         super(modelo, placa, valorDiaria);
+        this.cambioAutomatico = true;
+        this.computadorBordo = true;
+    }
+
+    @Override
+    public boolean ofereceServicoLuxo() {
+        return true; // Carro Premium oferece dois diferenciais do luxo
     }
 
     @Override
